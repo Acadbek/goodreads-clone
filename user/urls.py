1bf3bfb387cebd3a76d423e91da3676935718ab6
Asadbek <ibnnumon@gmail.com>
2023-11-11 10:35:12 +0500

    BPW: url pattern

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from user.views import RegisterView, LoginView
 
+app_name = 'user'
+
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login')
