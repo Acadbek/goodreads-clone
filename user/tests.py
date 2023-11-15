0a9fba23a0cc64efe8ca98740b35d15a4b94d2ab
Asadbek <ibnnumon@gmail.com>
2023-11-15 19:41:56 +0500

    unit tests

--- user/tests.py
+++ user/tests.py
@@ -35,10 +35,36 @@ class RegistrationTestCase(TestCase):
         )
 
         user_count = User.objects.count()
-        
+
         self.assertEqual(user_count, 0)
-        
+
         self.assertFormError(response, 'form', 'username',
                              'This field is required.')
         self.assertFormError(response, 'form', 'password',
                              'This field is required.')
+
+    def test_unique_username(self):
+        # 1. create a user
+        user = User.objects.create_user(username='liza', first_name='Monaliza')
+        user.set_password('liza')
+        user.save()
+
+        # 2. try to create another user with the same username
+        response = self.client.post(
+            reverse('user:register'),
+            data={
+                "username": 'Anna',
+                'first_name': 'Annaliza',
+                'last_name': 'Jakonda',
+                'email': 'liza@gmail.com',
+                'password': 'liza'
+            }
+        )
+
+        # 3. check that the second user was not created
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 1)
+
+        # 4. check that the form contains the error message
+        self.assertFormError(response, 'form', 'username',
+                             'A user with that username already exists.')
