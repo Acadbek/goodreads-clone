47f2d916741bce15b16230c37b8c24b923de063e
Asadbek <ibnnumon@gmail.com>
2023-11-22 12:43:53 +0500

    generic views

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
 from book.models import Book
 
 
@@ -10,15 +11,25 @@ def truncate_text(text, max_length=10):
     else:
         return text
 
+class BooksView(ListView):
+    template_name = 'book/list.html'
+    queryset = Book.objects.all()
+    context_object_name = 'books'
 
-class BooksView(LoginRequiredMixin, View):
-    def get(self, request):
-        books = Book.objects.all()
+# class BooksView(LoginRequiredMixin, View):
+#     def get(self, request):
+#         books = Book.objects.all()
 
-        return render(request, 'book/list.html', {'books': books})
+#         return render(request, 'book/list.html', {'books': books})
 
 
-class BookDetailView(View):
-    def get(self, request, id):
-        book = Book.objects.get(id=id)
-        return render(request, 'book/detail.html', {'book': book})
+# class BookDetailView(View):
+#     def get(self, request, id):
+#         book = Book.objects.get(id=id)
+#         return render(request, 'book/detail.html', {'book': book})
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book/detail.html'
+    pk_url_kwarg = 'id'
