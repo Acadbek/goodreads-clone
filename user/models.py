f0d530b43e9fff28f0133326d2c2c7787dc4d538
Asadbek <ibnnumon@gmail.com>
2023-11-22 18:28:48 +0500

    front: profile & change model

--- user/models.py
+++ user/models.py
@@ -3,4 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    profile_picture = models.ImageField(default="default_profile_pic.jpg")
+    profile_picture = models.ImageField(default="default.jpg")
+    job = models.CharField(max_length=100)
+    bio = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=100)
+    website = models.URLField(blank=True)
+    github = models.URLField(blank=True)
